6cbce9f554b67c5ccdf6c945954030a7e81882e5
Mohanty <2492354@cognizant.com>
2026-05-14 09:01:19 +0530

    Removed analytics-service from notification branch

--- analytics-service/src/main/java/com/travel360/analyticsservice/controller/AnalyticsController.java
+++
@@ -1,4 +0,0 @@
-package com.travel360.analyticsservice.controller;
-
-public class AnalyticsController {
-}
--- analytics-service/src/main/java/com/travel360/analyticsservice/repository/KPIReportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.travel360.analyticsservice.repository;
-
-import com.travel360.analyticsservice.entity.KPIReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KPIReportRepository extends JpaRepository<KPIReport, Long> {
-}
